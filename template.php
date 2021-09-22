b32bd37ce9e6885decf112793c07da6d2f6898d5
ng20001 <77092515+ng20001@users.noreply.github.com>
2021-09-22 15:05:54 +0900

    commit

---
+++ template.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>template.php</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" media="screen" href="/phpmotors/css/style.css">
+    </head>
+    <body>
+        <div class="wrap">
+            <header>
+                <img src="/phpmotors/images/site/logo.png" alt="PHPMotors logo">
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li class="active"><a href="#">Classic</a></li>
+                    <li><a href="#">Sports</a></li>
+                    <li><a href="#">SUV</a></li>
+                    <li><a href="#">Trucks</a></li>
+                    <li><a href="#">Used</a></li>
+                </ul>
+            </nav>
+            <main>
+            <h1>Content title here</h1>
+            
+            </main>
+            <footer>
+                <div class="footer-wrap">
+                    <hr>
+                    <p>&copy; PHP Motors, All rights reserved.</p>
+                    <p>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.</p>
+                    <p>Last Updated: 30 March, 2018</p>
+                </div>
+                
+            </footer>
+        </div>
+    </body>
+</html>
